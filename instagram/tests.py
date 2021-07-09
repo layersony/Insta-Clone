ddef19bdbfb5e435e3b539a8f70ac00033a5eea6
layersony <sammaingi5@gmail.com>
2021-07-09 23:18:58 +0300

    initial test location setup

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Location, Profile, Post
+
+
+class TestLocation(TestCase):
+  def setUp(self):
+    self.location = Location(location='Machakos')
+
+  def TearDown(self):
+    Location.objects.all().delete()
 
 # Create your tests here.
