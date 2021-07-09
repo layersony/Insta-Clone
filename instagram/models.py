a0ff5b9390a4e8c186fd04c9a75fb1625fe52559
layersony <sammaingi5@gmail.com>
2021-07-09 23:18:22 +0300

    methods for location add

--- instagram/models.py
+++ instagram/models.py
@@ -12,6 +12,16 @@ Gender = (
 class Location(models.Model):
   location = models.CharField(max_length=100)
 
+  def saveLocation(self):
+    self.save()
+
+  @classmethod
+  def deleteLocation(cls, id):
+    cls.objects.filter(id=id).delete()
+
+  @classmethod
+  def updateLocation(cls, id, locaUpdate):
+    cls.objects.filter(id=id).update(location=locaUpdate)
   def __str__(self):
     return self.location
 
