088b29df7726fe84447c9761f262d5c44d4da332
layersony <sammaingi5@gmail.com>
2021-07-11 23:49:12 +0300

    url add & styling

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,8 +6,11 @@ from django.conf import settings
 
 urlpatterns=[
   path('', views.index, name='home'),
-  path('accounts/profile', views.profile, name='uprofile'),
-  path('postpic', views.post_pic, name='postpic')
+  path('account/profile', views.profile, name='uprofile'),
+  path('postpic', views.post_pic, name='postpic'),
+  path('userprofile/<int:id>/', views.userprofile, name='userprofile'),
+  path('search/', views.searchUser, name='search_results'),
+  path('imagedetails/<int:id>', views.imagedetails, name='imagedetails')
 ]
 
 if settings.DEBUG:
--- instagram/views.py
+++ instagram/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post, Profile, Location, Comments
-from .form import UserForm, ProfileForm, PostPicForm, CommentForm
+from .form import UserForm, ProfileForm, PostPicForm, CommentForm, LikeForm
 from django.contrib import messages
 
 @login_required(login_url='accounts/login')
 def index(request):
   if request.method == 'POST':
     commentForm = CommentForm(request.POST)
+    likeform = LikeForm(request.POST)
     if commentForm.is_valid():
       pic_id = int(request.POST.get('imageid'))
       pic = Post.objects.get(id=pic_id)
@@ -15,22 +16,31 @@ def index(request):
       com.user = request.user
       com.pic = pic
       com.save()
+    
+    # if likeform.is_valid():
+    #   like_id = int(request.POST.get('imageid'))
+    #   likes = Post.objects.get(id=like_id)
+    #   like = commentForm.save(commit=False)
+    #   like.user = request.user
+    #   like.pic = likes
+    #   like.save()
+
     return redirect('/#image'+str(pic_id))
+
+    
   
   title = 'this is title'
   allpics = Post.all_pictures()
   commentForm = CommentForm()
+  likeform = LikeForm()
   allcomments = Comments.objects.all()
-  print(allcomments)
-  return render(request, 'index.html', {'title': title, 'allpics':allpics, 'commentForm':commentForm, 'allcomments':allcomments})
+  return render(request, 'index.html', {'title': title, 'allpics':allpics, 'commentForm':commentForm, 'allcomments':allcomments, 'likeform':likeform})
 
 @login_required(login_url='accounts/login')
 def profile(request):
-
   if request.method == 'POST':
     user_form = UserForm(request.POST, instance=request.user)
     profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
-
     if user_form.is_valid() and profile_form.is_valid():
       user_form.save()
       profile_form.save()
@@ -40,7 +50,6 @@ def profile(request):
   title = 'Profile'
   user_form = UserForm(instance=request.user)
   profile_form = ProfileForm(instance=request.user)
-
   user_pics = Post.user_pictures(request.user.username)
   return render(request, 'profile/index.html', {'title':title, "user":request.user, "user_form":user_form, 'profile_form':profile_form, 'user_pics':user_pics})
 
@@ -56,5 +65,34 @@ def post_pic(request):
     return redirect('uprofile')
 
   postForm = PostPicForm()
-  return render(request, 'profile/postpic.html', {'postForm':postForm})
+  return render(request, 'profile/postpic.html', {'postForm':postForm, 'user':request.user})
 
+def userprofile(request, id):
+  user = Profile.objects.get(id = id)
+  user_pics = Post.user_pictures(request.user.username)
+  return render(request, 'userprofile.html', {'userprofile':user, 'user_pics':user_pics})
+
+def searchUser(request):
+  if 'search' in request.GET and request.GET['search']:
+    search_term = request.GET.get('search')
+    searchprofiles = Profile.searchProfile(search_term)
+    return render(request, 'search.html', {'searchresults':searchprofiles})
+  else:
+    return redirect('home')
+
+def imagedetails(request, id):
+  if request.method == 'POST':
+    commentForm = CommentForm(request.POST)
+    if commentForm.is_valid():
+      pic_id = int(request.POST.get('imageid'))
+      pic = Post.objects.get(id=pic_id)
+      com = commentForm.save(commit=False)
+      com.user = request.user
+      com.pic = pic
+      com.save()
+    return redirect('imagedetails', id=id)
+
+  commentForm = CommentForm(request.POST)
+  pic = Post.objects.get(id = id)
+  allcomments = Comments.objects.all()
+  return render(request, 'imagedetails.html', {'specificpic':pic, 'commentForm':commentForm, 'allcomments':allcomments})
