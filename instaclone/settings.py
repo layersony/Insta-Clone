eefa5564cfd4c02b12d12019cea91d6f15c9d434
layersony <sammaingi5@gmail.com>
2021-07-12 00:34:57 +0300

    deploy to heruku

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'instaclone.urls'
